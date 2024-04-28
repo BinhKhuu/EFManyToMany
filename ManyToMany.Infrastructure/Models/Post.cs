13d597b6c48cf214861e272330d47de58a124ab9
binhk <binhkhuu90@gmail.com>
2024-04-28 12:59:51 +0800

    update comments

--- ManyToMany.Infrastructure/Models/Post.cs
+++ ManyToMany.Infrastructure/Models/Post.cs
@@ -6,5 +6,7 @@ namespace ManyToMany.Infrastructure.Models;
 public partial class Post
 {
     public int Id { get; set; }
+
+    // Navigation (skip) collection the new way to map many-to-many
     public virtual ICollection<Tag> Tags { get; set; }
 }
--- ManyToMany.Infrastructure/Models/Tag.cs
+++ ManyToMany.Infrastructure/Models/Tag.cs
@@ -6,5 +6,6 @@ namespace ManyToMany.Infrastructure.Models;
 public partial class Tag
 {
     public int Id { get; set; }
+    // Navigation (skip) collection the new way to map many-to-many
     public virtual ICollection<Post> Posts { get; set; }
 }
